369011a6c87596ac94674d27205ed98712ed0fcf
nicolo.depietro <nicolo.depietro@issgreppi.it>
2023-05-03 19:12:51 +0200

    aggiunt dbcontext e cose base

---
+++ ProgettoCinema/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using ProgettoCinema.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProgettoCinema.Data;
+public class ApplicationDbContext : DbContext
+{
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+
+    }
+
+    public DbSet<Utente> Utenti { get; set; } = null!;
+}

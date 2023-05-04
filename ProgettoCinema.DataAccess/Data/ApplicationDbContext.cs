cf9fd7be3490338e605230ad7f0f6727dd944b0e
NICOLO DE PIETRO <NICOLO.DEPIETRO@issgreppi.it>
2023-05-04 09:38:47 +0200

    fix visivi per la pagina di login

--- ProgettoCinema.DataAccess/Data/ApplicationDbContext.cs
+++ ProgettoCinema.DataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using ProgettoCinema.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace ProgettoCinema.DataAccess.Data;
-public class ApplicationDbContext : IdentityDbContext
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+
 {
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -11,10 +13,10 @@ public class ApplicationDbContext : IdentityDbContext
     }
 
     public DbSet<Utente> Utenti { get; set; } = null!;
-    //public DbSet<Film> Films { get; set; }    //DA VEDERE L'ORDINE IN CUI METTERLI
-    //public DbSet<Posto> Posti { get; set; }
-    //public DbSet<Sala> Sale { get; set; }
-    //public DbSet<Spettacolo> Spettacoli { get; set; }
+    //public DbSet<Film> Films { get; set; } = null!;   //DA VEDERE L'ORDINE IN CUI METTERLI
+    //public DbSet<Posto> Posti { get; set; } = null!;
+    //public DbSet<Sala> Sale { get; set; } = null!;
+    //public DbSet<Spettacolo> Spettacoli { get; set; } = null!;
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
